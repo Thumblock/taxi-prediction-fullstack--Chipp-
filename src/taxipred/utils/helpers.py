42c8ee32d61fbd639e2a842394a03ea0a315ff12
Chipp Larusson <chipp.larusson@hotmail.com>
2025-10-23 15:55:49 +0200

    minimal HTTP helpers, works now

---
+++ src/taxipred/utils/helpers.py
@@ -0,0 +1,10 @@
+import requests
+from urllib.parse import urljoin
+
+def read_api_endpoint(endpoint="/", base_url="http://127.0.0.1:8000"):
+    url = urljoin(base_url, endpoint)
+    return requests.get(url)
+
+def post_api_endpoint(endpoint="/", payload=None, base_url="http://127.0.0.1:8000"):
+    url = urljoin(base_url, endpoint)
+    return requests.post(url, json=(payload or {}))
