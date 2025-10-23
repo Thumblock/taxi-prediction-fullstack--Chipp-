dda44d4eb208e5e35b0e5e22553c2537393ca333
Chipp Larusson <chipp.larusson@hotmail.com>
2025-10-23 16:01:36 +0200

    Forgot to add timeout,without it, Streamlit can freeze if the API is slow or down

--- src/taxipred/utils/helpers.py
+++ src/taxipred/utils/helpers.py
@@ -3,8 +3,8 @@ from urllib.parse import urljoin
 
 def read_api_endpoint(endpoint="/", base_url="http://127.0.0.1:8000"):
     url = urljoin(base_url, endpoint)
-    return requests.get(url)
+    return requests.get(url, timeout=10)
 
 def post_api_endpoint(endpoint="/", payload=None, base_url="http://127.0.0.1:8000"):
     url = urljoin(base_url, endpoint)
-    return requests.post(url, json=(payload or {}))
+    return requests.post(url, json=(payload or {}), timeout=10)
